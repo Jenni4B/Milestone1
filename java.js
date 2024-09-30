54d50b98a90c66c3a5c87fcee43655490faebab8
Jenni4B <jennifer.ea.blocker@gmail.com>
2024-09-30 11:13:45 -0400

    deleted instructions file, no longer needed

---
+++ java.js
@@ -0,0 +1,35 @@
+let instagramLink = "https://www.instagram.com/jeanbeanbc/"
+let githubLink = "https://github.com/Jenni4B"
+
+
+function instagram(){
+    window.location.href = instagramLink;
+
+}
+function github(){
+    window.location.href = githubLink;
+
+}
+
+}
+let videos = [{
+    link: "https://www.youtube.com/watch?v=k5rEQ2wFPUw",
+    videoTitle: "a playlist for night studies (dark academia)",
+    description: "A video for late night studying. Make sure you drink water and stretch occasionally."
+    },
+   {
+    link: "https://www.youtube.com/embed/NAKOzMekf2c?si=CE0tMYVxvOyaqLP0",
+    videoTitle: "Roblox Halloween Event 2022",
+    description: "Like I said at the beginning of the video, I got a little busy (and possibly a bit demotivated) and didn't feel like uploading I didn't even play Roblox much at all Thanks for sticking around and having patience! Enjoy!"
+   },
+   {
+    link: "https://www.youtube.com/watch?v=oSVPkuplVRY",
+    videoTitle: "I Built a PC that Makes Coffee",
+    description: "I turned my computer into an actual working coffee machine!"
+   },
+   {
+    link: "https://www.youtube.com/watch?v=GVPAjftmqr4",
+    videoTitle: "Just trying to make some Cheese Cake..."
+    description: "Cheese Cake | Sweet Potato Cheese Cake | 고구마 치즈 케이크 Today's menu is Sweet Potato Cheese Cake. It's relatively simple to make but it takes some time to cook."
+   }
+]
